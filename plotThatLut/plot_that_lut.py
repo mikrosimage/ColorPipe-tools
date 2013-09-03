3ab5d8ceae587bce77a53b8145fa722e1ca1d122
mfe <mfe@mikrosimage.eu>
2013-09-03 15:03:28 +0200

    Rename test file dir #1

--- plotThatLut/plot_that_lut.py
+++ plotThatLut/plot_that_lut.py
@@ -203,12 +203,12 @@ def plot_cube(lutfile, cube_size, processor):
 
 
 def test_lut_1d():
-    lutfile = "testFiles/identity.csp"
+    lutfile = "test_files/identity.csp"
     plot_curve(lutfile, samples_count=DEFAULT_SAMPLE)
 
 
 def test_lut_3d():
-    lutfile = "testFiles/identity.3dl"
+    lutfile = "test_files/identity.3dl"
     plot_cube(lutfile, cube_size=DEFAULT_CUBE_SIZE)
 
 
