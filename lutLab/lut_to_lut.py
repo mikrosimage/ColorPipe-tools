2389dd46c42bd0a218efcfce4180b3dfd37249ba
mfe <mfe@mikrosimage.eu>
2013-10-18 18:19:35 +0200

    Add 3D cube export to lut_to_lut #6

--- lutLab/lut_to_lut.py
+++ lutLab/lut_to_lut.py
@@ -8,8 +8,11 @@
 import argparse
 from utils.ocio_helper import OCIO_LUTS_FORMATS, create_ocio_processor
 from utils.csp_helper import write_2d_csp_lut
-from utils.cube_helper import write_2d_cube_lut
+from utils.cube_helper import write_2d_cube_lut, write_3d_cube_lut
 from utils.lut_utils import get_default_out_path
+from PyOpenColorIO.Constants import (
+    INTERP_LINEAR, INTERP_TETRAHEDRAL
+)
 
 
 class LutToLutException(Exception):
@@ -17,7 +20,7 @@ class LutToLutException(Exception):
 
 
 def lut_to_lut(inlutfile, outlutfile=None, type='1D_CUBE',
-               lutsize=16):
+               lutsize=16, cubesize=17):
     """Extract the tone mapping curve of a 3D LUT
 
     Args:
@@ -30,35 +33,45 @@ def lut_to_lut(inlutfile, outlutfile=None, type='1D_CUBE',
         type (str): specify output LUT format. For now only 2D csp and 2D cube
         are available.
 
-        lutsize (int): out LUT bit precision. Ex : 16 (bits)
+        lutsize (int): out LUT bit precision for 1D. Ex : 16 (bits)
 
     """
     samples_count = pow(2, lutsize)
     if type == '1D_CUBE':
         ext = ".cube"
         write_function = write_2d_cube_lut
+        interp = INTERP_LINEAR
+    elif type == '3D_CUBE':
+        ext = ".cube"
+        write_function = write_3d_cube_lut
+        interp = INTERP_TETRAHEDRAL
     elif type == '1D_CSP':
         ext = ".csp"
         write_function = write_2d_csp_lut
+        interp = INTERP_LINEAR
     else:
         raise LutToLutException("Unsupported export format!")
     if not outlutfile:
         outlutfile = get_default_out_path(inlutfile, ext)
-    processor = create_ocio_processor(inlutfile)
-        # init vars
+    processor = create_ocio_processor(inlutfile, interpolation=interp)
+    # init vars
     max_value = samples_count - 1.0
     red_values = []
     green_values = []
     blue_values = []
-    # process color values
-    for n in range(0, samples_count):
-        x = n/max_value
-        res = processor.applyRGB([x, x, x])
-        red_values.append(res[0])
-        green_values.append(res[1])
-        blue_values.append(res[2])
-    # write
-    write_function(outlutfile, red_values, green_values, blue_values)
+    if "1D" in type:
+        # process color values
+        for n in range(0, samples_count):
+            x = n/max_value
+            res = processor.applyRGB([x, x, x])
+            red_values.append(res[0])
+            green_values.append(res[1])
+            blue_values.append(res[2])
+        # write
+        write_function(outlutfile, red_values, green_values, blue_values)
+    elif "3D" in type:
+        # write
+        write_function(outlutfile, cubesize, processor)
 
 
 def __get_options():
@@ -83,11 +96,16 @@ def __get_options():
     parser.add_argument("-t", "--out-type",
                         help=("Output LUT type."),
                         type=str,
-                        choices=['1D_CSP', '1D_CUBE'], default='1D_CUBE')
+                        choices=['1D_CSP', '1D_CUBE', '3D_CUBE'],
+                        default='1D_CUBE')
     # out lut size
-    parser.add_argument("-os", "--outlut-size", help=(
+    parser.add_argument("-os", "--out-lut-size", help=(
         "Output lut bit precision. Ex : 10, 16, 32."
     ), default=16, type=int)
+    # out cube size
+    parser.add_argument("-ocs", "--out-cube-size", help=(
+        "Output cube size (3D only). Ex : 17, 32."
+    ), default=17, type=int)
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -95,4 +113,4 @@ if __name__ == '__main__':
     """
     args = __get_options()
     lut_to_lut(args.inlutfile, args.outlutfile, args.out_type,
-               args.outlut_size)
+               args.out_lut_size, args.out_cube_size)
