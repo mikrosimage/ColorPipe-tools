33539ff8504e2f30cbafdbbca2338506a8853904
Marie <marie@rodeofx.com>
2015-04-14 20:35:56 -0400

    lutLab: update test (#17)

--- test/general_test.py
+++ test/general_test.py
@@ -6,7 +6,7 @@ from plotThatLut import plot_that_lut
 import os
 import tempfile
 import shutil
-from lutLab import rgb_to_xyz_matrix
+from utils.colors_helper import get_colorspace_matrix, get_RGB_to_RGB_matrix
 
 
 DISPLAY = False
@@ -45,12 +45,24 @@ class GeneralTest(unittest.TestCase):
             # TODO
             pass
 
-    def test_rgb_to_matrix(self):
-        """Display rgb matrix
+    def test_colorspace_matrices(self):
+        """Test matrix conversions
 
         """
-        rgb_to_xyz_matrix.display_matrix('Rec709', 'spimtx')
-        # TODO test values
+        ACES_to_XYZ = [[0.95255239593818575, 0.0, 9.3678631660468553e-05],
+                       [0.34396644976507507, 0.72816609661348575, -0.072132546378560786],
+                       [0.0, 0.0, 1.0088251843515859]]
+
+        XYZ_to_ACES = [[1.0498110174979742, 0.0, -9.7484540579252874e-05],
+                       [-0.49590302307731976, 1.3733130458157063, 0.098240036057309993],
+                       [0.0, 0.0, 0.99125201820049902]]
+        ACES_to_Rec2020 = [[1.5128613853114372, -0.2589874063019148, -0.22978603267468098],
+                           [-0.079036464595355627, 1.1770668323294038, -0.10075565571179679],
+                           [0.0020912324769753847, -0.03114411050570343, 0.95350416068074784]]
+
+        self.assertEqual(ACES_to_XYZ, get_colorspace_matrix("ACES").tolist())
+        self.assertEqual(XYZ_to_ACES, get_colorspace_matrix("ACES", inv=True).tolist())
+        self.assertEqual(ACES_to_Rec2020, get_RGB_to_RGB_matrix("ACES", 'Rec2020_12bits').tolist())
 
     def tearDown(self):
         # Remove test directory
